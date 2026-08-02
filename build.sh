564fee61f25fa290adb73b286a837049ca4582bf
Chris Kaiser <chris.kaiser@bahmut.de>
2026-08-02 20:39:38 +0200

    Fixed linux dist filename

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ mkdir bin/linux
 odin build src -define:RELEASE=true -o:speed -out:bin/linux/pdx-flag-builder
 cp -r assets/ bin/linux/
 cd bin/linux/
-tar --exclude='*.zip' -a -cf 'pdx-flag-builder_x.x.x_windows_amd64.zip' *
-cd ..\..
+tar --exclude='*.zip' -a -cf 'pdx-flag-builder_x.x.x_linux_amd64.zip' *
