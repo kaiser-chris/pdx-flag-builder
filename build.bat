f1a9de533087f31f83e55de59451904e3cb3bf1b
Chris Kaiser <chris.kaiser@bahmut.de>
2026-08-02 20:35:51 +0200

    Improved release scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
-odin build src -define:RELEASE=true -o:speed -subsystem:windows -out:bin/pdx-flag-builder.exe -resource:resources.rc
-robocopy assets\ bin\assets\ /E /NJH /NJS
+mkdir bin\
+mkdir bin\windows
+odin build src -define:RELEASE=true -o:speed -subsystem:windows -out:bin/windows/pdx-flag-builder.exe -resource:resources.rc
+robocopy assets\ bin\windows\assets\ /E /NJH /NJS
 del resources.res
+cd bin\windows
+del "pdx-flag-builder_x.x.x_windows_amd64.zip"
+tar.exe --exclude="*.zip" -a -cf "pdx-flag-builder_x.x.x_windows_amd64.zip" "*"
+cd ..\..
--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-odin build src -define:RELEASE=true -o:speed -out:bin/pdx-flag-builder
-cp -r assets/ bin/
+mkdir bin/
+mkdir bin/linux
+odin build src -define:RELEASE=true -o:speed -out:bin/linux/pdx-flag-builder
+cp -r assets/ bin/linux/
+cd bin/linux/
+tar --exclude='*.zip' -a -cf 'pdx-flag-builder_x.x.x_windows_amd64.zip' *
+cd ..\..
